be9d9a66ff3defe32584ccae06eb21ca9ca06b13
AlexanderSemenov92 <42918575+AlexanderSemenov92@users.noreply.github.com>
2019-03-18 20:04:25 +0200

    --- Auto-Git Commit ---

--- src/js/script.js
+++ src/js/script.js
@@ -1,13 +1,14 @@
 $(document).ready(function () {
    
     $('.burger').click(function () {
-        if ($('.header-nav').css('display','none')) {
-            
-        }
+        if ($(this).next('.header-nav').css('display') == 'none') {
+            $(this).next('.header-nav').slideDown();
+            $('.burger').addClass("burger-open");
+        } 
         else {
-
+            $('.burger').removeClass("burger-open");
+            $(this).next('.header-nav').slideUp();
         }
-       $('.header-nav').slideDown();
     });
 
 });
