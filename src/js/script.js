1910d1ba0f6e23f5f676975c22c2db207ec7084e
AlexanderSemenov92 <42918575+AlexanderSemenov92@users.noreply.github.com>
2019-03-18 19:03:11 +0200

    --- Auto-Git Commit ---

--- src/js/script.js
+++ src/js/script.js
@@ -1,7 +1,13 @@
 $(document).ready(function () {
    
     $('.burger').click(function () {
-        
+        if ($('.header-nav').css('display','none')) {
+            
+        }
+        else {
+
+        }
+       $('.header-nav').slideDown();
     });
 
 });
