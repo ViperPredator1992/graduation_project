3badcf57491aafe7095dddfb09c395b7c9d90655
AlexanderSemenov92 <42918575+AlexanderSemenov92@users.noreply.github.com>
2019-03-20 18:33:22 +0200

    --- Auto-Git Commit ---

--- src/js/script.js
+++ src/js/script.js
@@ -63,4 +63,37 @@ $(document).ready(function () {
         nextArrow: $('.arrows__right')
     });
 
+    $('.responsive').slick({
+        dots: false,
+        infinite: false,
+        speed: 300,
+        slidesToShow: 5,
+        slidesToScroll: 1,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 1,
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ],
+        prevArrow: $('.left__arrow'),
+        nextArrow: $('.right__arrow')
+    });
+
 });
