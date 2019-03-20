fc95ed1bda11cb30eb176e63a5ca9c2d37a1bf0c
AlexanderSemenov92 <42918575+AlexanderSemenov92@users.noreply.github.com>
2019-03-20 22:57:49 +0200

    --- Auto-Git Commit ---

--- src/js/script.js
+++ src/js/script.js
@@ -96,4 +96,13 @@ $(document).ready(function () {
         nextArrow: $('.arrows-right__arrow')
     });
 
+    $(".map-show-on-click").click(function () {
+        var map = $(this).attr("data-map");
+        var map_width = $(window).width();
+        var map_height = $(this).attr("data-map-height");
+        $(this).css("height", map_height);
+        if (map_width > window) { map_width = 640; }
+        $(this).html('<iframe src="' + map + '" width="' + map_width + '" height="' + map_height + '" frameborder="0" style="border:0" allowfullscreen></iframe>');
+    }); 
+
 });
